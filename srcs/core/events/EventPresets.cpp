8c60aea701649d444476374376c1e623d58dfe09
dolovnyak <dolovnyak@yandex.ru>
2022-08-13 19:02:36 +0300

    big thoughts http proceesing

--- srcs/core/events/EventPresets.cpp
+++ srcs/core/events/EventPresets.cpp
@@ -7,25 +7,55 @@ Event EventPresets::ParseHttpRequest(const std::shared_ptr<Connection>& connecti
 
     auto parse_http = [connection, raw_request, event_queue](){
         Request request = HttpModule::ProcessParseHttpRequest(connection, raw_request);
-        LOG_INFO("request was parsed, try to push in queue");
         event_queue->push(EventPresets::MakeHttpResponse(connection, request));
-        LOG_INFO("push in queue successfully");
     };
 
     return {EventType::ParseHttpRequest, parse_http};
 }
 
+//Event EventPresets::ParseHttpRequest(const std::shared_ptr<Connection>& connection,
+//                                     const std::shared_ptr<std::string>& raw_request,
+//                                     std::queue<Event>* event_queue) {
+//
+//    auto parse_http = [connection, raw_request, event_queue](){
+//        event_queue->push(EventPresets::MakeHttpResponse(connection, fut));
+//    };
+//
+//    return {EventType::ParseHttpRequest, parse_http};
+//}
+
+//Event EventPresets::WaitResult(const std::shared_ptr<Connection>& connection,
+//                                     const std::shared_ptr<std::string>& raw_request,
+//                                     std::queue<Event>* event_queue) {
+//
+//    auto parse_http = [connection, raw_request, std::future fut](){
+//        if (fut.ready) {
+//            event_queue->push(EventPresets::MakeHttpResponse(connection, fut));
+//        }
+//        else {
+//            event_queue->push(EventPresets::WaitResult(connection, raw_request, event_queue));
+//        }
+////        event_queue->push(EventPresets::MakeHttpResponse(connection, request));
+//    };
+//
+//    return {EventType::ParseHttpRequest, parse_http};
+//}
+
 Event EventPresets::MakeHttpResponse(const std::shared_ptr<Connection>& connection, Request request) {
     auto make_response = [connection, request](){
-        std::string response;
-        if (request.body.empty()) {
-            response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Hello, World!</h1></body></html>";
-        } else {
-            response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Hello," + request.body + "!</h1></body></html>";
-        }
+        std::string response = request.body;
         connection->SetResponse(response);
         connection->SetWriteEventToCoreModule();
     };
 
     return {EventType::MakeHttpResponse, make_response};
 }
+
+/// EventFSM
+/// InitState -> ParseHttpRequest
+/// ParseHttpRequest -> ParseHttpRequestWaitResult
+/// ParseHttpRequestWaitResult -> ParseHttpRequestWaitResult
+/// ParseHttpRequestWaitResult -> MakeHttpResponse
+/// MakeHttpResponse -> MakeHttpResponseWaitResult
+/// MakeHttpResponseWaitResult -> MakeHttpResponseWaitResult
+/// MakeHttpResponseWaitResult -> WriteToSocket
